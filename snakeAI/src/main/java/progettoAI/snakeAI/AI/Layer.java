bfd51b9f4b0188c4b99e542e2c8dd50140819fe0
DavideFiorentini666 <d.fiorentini1@studenti.unibg.it>
2025-12-01 21:11:21 +0100

    aggiornamento commenti

--- snakeAI/src/main/java/progettoAI/snakeAI/AI/Layer.java
+++ snakeAI/src/main/java/progettoAI/snakeAI/AI/Layer.java
@@ -125,10 +125,10 @@ public abstract class Layer {
 	 * @param backLayer
 	 */
 	public void derivateCalculus(Layer backLayer) {
-		RealMatrix tmp = Tools.outerProduct(this.derivateFromActivationToPreActivation(), this.derivateFromLossToActivation);//calculus from loss to weigths
+		RealMatrix tmp = Tools.outerProduct(this.derivateFromActivationToPreActivation(), this.derivateFromLossToActivation);//calculus derivate from loss to weigths
 		backLayer.derivateFromLossToWeights.add(backLayer.derivateFromPreActivationToWeightsCalculus().multiply(tmp));
 		
-		this.derivatoFromLossToBias.add(this.derivateFromActivationToPreActivation().ebeMultiply(this.derivateFromLossToActivation));//calculus from loss to bias
+		this.derivatoFromLossToBias.add(this.derivateFromActivationToPreActivation().ebeMultiply(this.derivateFromLossToActivation));//calculus derivate from loss to bias
 	}
 	
 	public void derivateFromLossToActivationCalculus() {//TODO
