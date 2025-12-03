782f790e01d0e6fc95d687dd98c9c3d914b15529
manufiore03 <fiorentini.manuela.03@gmail.com>
2025-12-03 19:33:10 +0100

    Update ThreadAgent.java

--- snakeAI/src/main/java/thread/ThreadAgent.java
+++ snakeAI/src/main/java/thread/ThreadAgent.java
@@ -1,5 +1,8 @@
 package thread;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import enumSnake.Snake;
 import model.*;
 import snakeGame.*;
@@ -39,7 +42,7 @@ public class ThreadAgent extends Thread{
 	 * @param endDegree
 	 * @return if the object collides with one of the line of the head it will return true, else false
 	 */
-	public boolean objectCollide(int k, int h, Map map, SnakeBox snakeHead, double startDegree, double endDegree) {
+	public boolean objectCollide(int k, int h, Map map, SnakeBox snakeHead, String dir) {
 		
 		int deltaX = map.getMap()[k][h].getX() - snakeHead.getX();
 		int deltaY = map.getMap()[k][h].getY() - snakeHead.getY();
@@ -54,15 +57,28 @@ public class ThreadAgent extends Thread{
 				
 		double alphaDegree = Math.toDegrees(alpha); //transform in degree
 				
-		if(alphaDegree > startDegree && alphaDegree < endDegree) {
+		if(dir == "up" && (alphaDegree<=180 && alphaDegree>=0)) {
+			return true;
+		}else if(dir == "down" && (alphaDegree>=-180 && alphaDegree<=0)) {
+			return true;
+		}else if(dir == "left" && (alphaDegree>=90 && alphaDegree<=-90)) {
+			return true;
+		}else if(dir == "right" && (alphaDegree>=-90 && alphaDegree<=90)) {
 			return true;
 		}
-	
+		
 		return false;
 	}
 	
-	
-	public double calculateDistance(Box box, SnakeBox snakeHead, double startDegree, double endDegree) {
+	/**
+	 * 
+	 * @param box
+	 * @param snakeHead
+	 * @param startDegree
+	 * @param endDegree
+	 * @return the distance between an object and the head of the snake
+	 */
+	public double calculateDistance(Box box, SnakeBox snakeHead) {
 		
 		double x = Math.abs(snakeHead.getX() - box.getX());
 		double y = Math.abs(snakeHead.getY() - box.getY());
@@ -71,13 +87,15 @@ public class ThreadAgent extends Thread{
 		return distance;
 	}
 	
-	
 	/**
+	 * l'array avrà nella prima parte i valori distanza dal muro, nella parte centrale la mela, nella parte finale il corpo
 	 * 
 	 * @param map
 	 * @return normalization of the distance of the snake's head and the elements
 	 */
-	public double[] mapConversion(Map map) {
+	public List<Double> mapConversion(Map map) {
+		
+		List<Double> risultati = new ArrayList<>();
 		
 		for(int i=0; i<map.getMap().length; i++) { //i get the length of the rows
 			for(int j=0; j<map.getMap()[0].length; j++) { //i get the length of the columns
@@ -87,27 +105,33 @@ public class ThreadAgent extends Thread{
 					SnakeBox snakeHead = (SnakeBox) map.getMap()[i][j];
 					SnakeBox snakeFirstBodyBox = snakeHead.getNext(1);
 
+					String dir;
+					
 					//i calculate the direction of the snake
 					if(snakeHead.getX() - snakeFirstBodyBox.getX() < 0) {
 						//head left and body right
-						//range from 270° to 90° CLOCKWISE
+						
 						for(int k=0; k<map.getMap().length; k++) {
 							for(int h=0; h<map.getMap()[0].length; h++) {
-								//INSERIRE OBJECT COLLIDE E CALCULATE DISTANCE
+								
+								dir = "left";
+								if (objectCollide(k, h, map, snakeHead, dir) == true) {
+									
+									risultati.add(calculateDistance(map.getMap()[k][h], snakeHead));
+									//SISTEMA PERCHE NELLA PRIMA PARTE CI SIAMO MURI POI MELA POI CORPO
+								}
+								
 							}
 						}
 						
 					} else if(snakeHead.getX() - snakeFirstBodyBox.getX() > 0) {
 						//head right and body left
-						//range from 90° to 270° CLOCKWISE
 						
 					} else if(snakeHead.getY() - snakeFirstBodyBox.getY() < 0) {
 						//head down and body up
-						//range from 360° to 180° CLOCKWISE
 						
 					} else {
 						//head up and body down
-						//range from 180° to 0° CLOCKWISE
 						
 					}
 				}
