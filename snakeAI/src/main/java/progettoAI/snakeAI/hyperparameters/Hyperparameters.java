0bd888e5f2db2af57f4ee68fc9e5a851c054518e
DavideFiorentini666 <d.fiorentini1@studenti.unibg.it>
2025-12-06 17:28:20 +0100

    cambiato iperparametro

--- snakeAI/src/main/java/progettoAI/snakeAI/hyperparameters/Hyperparameters.java
+++ snakeAI/src/main/java/progettoAI/snakeAI/hyperparameters/Hyperparameters.java
@@ -4,7 +4,7 @@ public final class Hyperparameters {
 	public static double alphaW = 0.3;
 	public static double alphaB = 0.3;
 	public static int epoche = 10;
-	public static int minibacthSize = 50;
+	public static double minibacthSize = 0.8;
 	public static double discount = 0.99;
 	public static double gamma = 0.95; 
 	public static int timeStep = 30;
