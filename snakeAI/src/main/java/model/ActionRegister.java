ed6525ebdd1838946570c95b225894faf09eea07
DavideFiorentini666 <d.fiorentini1@studenti.unibg.it>
2025-12-07 15:17:15 +0100

    creata classe ActionRegister

--- snakeAI/src/main/java/model/ActionRegister.java
+++ snakeAI/src/main/java/model/ActionRegister.java
@@ -2,7 +2,22 @@ package model;
 
 public class ActionRegister {
 
+	public double[] actionsProb;
+	public int indexAction;
+	public double[] state;
+	public double vTarget;
+	public double vEstimated;
+	public double advantage;
+	public float reward;
+	
 	public ActionRegister() {
+		actionsProb = null;
+		indexAction = -1;
+		state = null;
+		vTarget = 0;
+		vEstimated = 0;
+		advantage = 0;
+		reward = 0;
 		
 	}
 	
