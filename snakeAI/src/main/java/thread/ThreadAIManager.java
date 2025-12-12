bf9b6759be7083bbc40c860c459ff5f7779cf43a
manufiore03 <fiorentini.manuela.03@gmail.com>
2025-12-12 14:47:13 +0100

    Update ThreadAIManager.java

--- snakeAI/src/main/java/thread/ThreadAIManager.java
+++ snakeAI/src/main/java/thread/ThreadAIManager.java
@@ -1,6 +1,10 @@
 package thread;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+
+import fileManager.JsonFileManager;
 
 public class ThreadAIManager {
 
@@ -15,10 +19,14 @@ public class ThreadAIManager {
 	}
 	
 	public void createIstance(){
+	
+		String relPath = "target/modelli/";
+		Path relativePath = Paths.get(relPath);
+		Path absolutePath = relativePath.toAbsolutePath();
 		
 		for(int i = 0; i<this.threadsModelNumber; i++) {
 			
-			ThreadModel threadModel = new ThreadModel("../modelli/modello.Json");
+			ThreadModel threadModel = new ThreadModel(absolutePath.toString());
 			threadModels.add(threadModel);
 			
 			for(int j = 0; j<this.threadsAgentNumber; j++) {
@@ -26,6 +34,8 @@ public class ThreadAIManager {
 				threadAgents.add(threadAgent);
 			}
 		}
+		
+		
 	
 	}
 	
