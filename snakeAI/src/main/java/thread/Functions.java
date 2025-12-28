31920ba71e3742d9ff1d424a5b76ee4a85343f6d
manufiore03 <fiorentini.manuela.03@gmail.com>
2025-12-28 11:35:04 +0100

    Update Functions.java

--- snakeAI/src/main/java/thread/Functions.java
+++ snakeAI/src/main/java/thread/Functions.java
@@ -15,8 +15,7 @@ public interface Functions {
 		double[] array = new double[n];
 		
 		for(int i = 0; i<array.length; i++) {
-			array[i] = -1; //TODO, COME INIZIALIZZO CON CHE VALORE???
-							//perche chiedi a davide del perche si ha che a priori distanza normalizzata 0 (anche quando non ce oggetto)
+			array[i] = -1; 
 		}
 		
 		return array;
