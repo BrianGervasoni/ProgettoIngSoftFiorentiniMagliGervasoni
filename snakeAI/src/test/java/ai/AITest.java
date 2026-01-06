44c51eaca0bf1b32942c176aa90ee157eb2b9cb2
DavideFiorentini666 <d.fiorentini1@studenti.unibg.it>
2026-01-06 15:26:51 +0100

    corretti test

--- snakeAI/src/test/java/ai/AITest.java
+++ snakeAI/src/test/java/ai/AITest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.nd4j.linalg.factory.Nd4j;
 
+import errorHandler.ArithmeticException;
 import model.ActionRegister;
 import progettoAI.snakeAI.AI.*;
 import progettoAI.snakeAI.hyperparameters.Hyperparameters;
@@ -12,58 +13,63 @@ public class AITest {
 
 	@Test
 	void testBackProp() {
-		double[] b = new double[] {1,2};
-		double[][] w = new double[2][3];
-		for(int i=0; i< w.length;i++) {//minibatch = 1, node=2, inputNode=3
-			for(int j=0; j<w[0].length;j++) {//create matrix = [0,1,2],[1,2,3]
-				w[i][j] = i+j;
+		try {
+			double[] b = new double[] {1,2};
+			double[][] w = new double[2][3];
+			for(int i=0; i< w.length;i++) {//minibatch = 1, node=2, inputNode=3
+				for(int j=0; j<w[0].length;j++) {//create matrix = [0,1,2],[1,2,3]
+					w[i][j] = i+j;
+				}
 			}
+			
+			double[] b2 = new double[] {1};
+			double[][] w2 = new double[1][3];
+			for(int i=0; i< w.length;i++) {
+				for(int j=0; j<w[0].length;j++) {//create matrix = [0,1,2]
+					w[i][j] = i+j;
+				}
+			}
+			
+			ActionRegister r = new ActionRegister();
+			r.state = new double [] {1,0,1};
+			LayerSoftMax softMax = new LayerSoftMax(b,w);
+			LayerIdentityFunction id = new LayerIdentityFunction(b2,w2);
+			AI ai = new AIActor(new LayerSoftMax[] {softMax},TypeGradientUpdate.DESCEND);
+			AI crit = new AICritic(new LayerIdentityFunction[] {id},TypeGradientUpdate.ASCEND);
+			r.actionsProb = ai.forwarding(r.state);
+			r.indexAction = r.actionsProb.length-1;
+			r.advantage = 2;
+			r.reward = 4;
+			r.vEstimated = crit.forwarding(r.state)[0];
+			r.vTarget = 3;
+			
+			ai.initBackPropagation();
+			crit.initBackPropagation();
+			ai.backPropagation(new ActionRegister[] {r});
+			crit.backPropagation(new ActionRegister[] {r});
+			
+			ai.optimization();
+			crit.optimization();
+			
+			double[] trueBias = new double[] {1.0081260267289072,1.9918739732710928};
+			double[][] trueWeights = new double[][] {
+				{ 0.0081260267289072,1.0000,2.0081260267289074},
+				{0.9918739732710928,2.0000,2.991873973271093},
+			};
+			
+			double[] trueBiasCrit = new double [] {-0.19999999999999996};
+			double[][] trueWeightsCrit = new double [][] {
+					{-1.2,0.0,-1.2}
+			};
+			
+			assertArrayEquals(trueBiasCrit,crit.getLayer().get(0).getBias().toDoubleVector());
+			assertArrayEquals(trueWeightsCrit,crit.getLayer().get(0).getWeights().toDoubleMatrix());
+			assertArrayEquals(trueBias,ai.getLayer().get(0).getBias().toDoubleVector());
+			assertArrayEquals(trueWeights,ai.getLayer().get(0).getWeights().toDoubleMatrix());
+		}catch(ArithmeticException e) {
+			e.printStackTrace();
 		}
 		
-		double[] b2 = new double[] {1};
-		double[][] w2 = new double[1][3];
-		for(int i=0; i< w.length;i++) {
-			for(int j=0; j<w[0].length;j++) {//create matrix = [0,1,2]
-				w[i][j] = i+j;
-			}
-		}
-		
-		ActionRegister r = new ActionRegister();
-		r.state = new double [] {1,0,1};
-		LayerSoftMax softMax = new LayerSoftMax(b,w);
-		LayerIdentityFunction id = new LayerIdentityFunction(b2,w2);
-		AI ai = new AIActor(new LayerSoftMax[] {softMax},TypeGradientUpdate.DESCEND);
-		AI crit = new AICritic(new LayerIdentityFunction[] {id},TypeGradientUpdate.ASCEND);
-		r.actionsProb = ai.forwarding(r.state);
-		r.indexAction = r.actionsProb.length-1;
-		r.advantage = 2;
-		r.reward = 4;
-		r.vEstimated = crit.forwarding(r.state)[0];
-		r.vTarget = 3;
-		
-		ai.initBackPropagation();
-		crit.initBackPropagation();
-		ai.backPropagation(new ActionRegister[] {r});
-		crit.backPropagation(new ActionRegister[] {r});
-		
-		ai.optimization();
-		crit.optimization();
-		
-		double[] trueBias = new double[] {1.0081260267289072,1.9918739732710928};
-		double[][] trueWeights = new double[][] {
-			{ 0.0081260267289072,1.0000,2.0081260267289074},
-			{0.9918739732710928,2.0000,2.991873973271093},
-		};
-		
-		double[] trueBiasCrit = new double [] {-0.19999999999999996};
-		double[][] trueWeightsCrit = new double [][] {
-				{-1.2,0.0,-1.2}
-		};
-		
-		assertArrayEquals(trueBiasCrit,crit.getLayer().get(0).getBias().toDoubleVector());
-		assertArrayEquals(trueWeightsCrit,crit.getLayer().get(0).getWeights().toDoubleMatrix());
-		assertArrayEquals(trueBias,ai.getLayer().get(0).getBias().toDoubleVector());
-		assertArrayEquals(trueWeights,ai.getLayer().get(0).getWeights().toDoubleMatrix());
 	}
 
 }
--- snakeAI/src/test/java/ai/ModelTest.java
+++ snakeAI/src/test/java/ai/ModelTest.java
@@ -10,6 +10,7 @@ import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 import org.nd4j.linalg.factory.Nd4j;
 
+import errorHandler.ArithmeticException;
 import fileManager.JsonFileManager;
 import model.ActionRegister;
 import model.Model;
@@ -19,33 +20,38 @@ public class ModelTest {
 
 	@Test
 	void testSaveAndLoad() {
-		//AICritic critic = new AICritic(new int[] {4,4,4,1},TypeGradientUpdate.DESCEND);
-		//AIActor actor = new AIActor(new int[] {4,5,5,4},TypeGradientUpdate.ASCEND);
-		//Model mSave = new Model(critic,actor);
-		Model m = null;
-		
-		String relPath = "modelli/modelloTest.json";
-		Path relativePath = Paths.get(relPath);
-		Path absolutePath = relativePath.toAbsolutePath();
-		//JsonFileManager.saveModel(mSave, absolutePath.toString());
 		try {
-			m = JsonFileManager.loadModel(absolutePath.toString());
-		}catch(IOException e) {
+			//AICritic critic = new AICritic(new int[] {4,4,4,1},TypeGradientUpdate.DESCEND);
+			//AIActor actor = new AIActor(new int[] {4,5,5,4},TypeGradientUpdate.ASCEND);
+			//Model mSave = new Model(critic,actor);
+			Model m = null;
+			
+			String relPath = "modelli/modelloTest.json";
+			Path relativePath = Paths.get(relPath);
+			Path absolutePath = relativePath.toAbsolutePath();
+			//JsonFileManager.saveModel(mSave, absolutePath.toString());
+			try {
+				m = JsonFileManager.loadModel(absolutePath.toString());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			
+			ActionRegister r= m.forwarding(new double[] {1,2,3,4});
+			r.reward = 4;
+			r.indexAction = 2;
+			
+			m.memorizeActions(new ActionRegister[] {r});
+			m.initBackPropagation();
+			
+			double[] test =  m.backPropagation();
+			
+			double[] trueLoss = new double[] {4.652735277428321,15.682484034489768};
+			assertArrayEquals(trueLoss,test);
+			assertNotNull(m.getMemory());
+		}catch(ArithmeticException e) {
 			e.printStackTrace();
 		}
 		
-		ActionRegister r= m.forwarding(new double[] {1,2,3,4});
-		r.reward = 4;
-		r.indexAction = 2;
-		
-		m.memorizeActions(new ActionRegister[] {r});
-		m.initBackPropagation();
-		
-		double[] test =  m.backPropagation();
-		
-		double[] trueLoss = new double[] {4.652735277428321,15.682484034489768};
-		assertArrayEquals(trueLoss,test);
-		assertNotNull(m.getMemory());
 	}
 	
 	@Test
